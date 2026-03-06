9857f53ca8f8e8c01061695d202df68005f5af3a
Gustav-Gut <gustavo.gutierrezy@gmail.com>
2026-03-06 01:11:45 -0300

    [ADD] New component Login

---
+++ src/app/features/auth/pages/login/login.ts
@@ -0,0 +1,28 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login.html',
+  styleUrls: ['./login.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class Login {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      console.log('Login attempt:', this.loginForm.value);
+    }
+  }
+}
